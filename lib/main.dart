7ebbd6cd9cd918b00b7a55d8e5858044337f59e4
Vinicius <vinicius@iMac-de-Vinicius.local>
2020-06-28 18:15:52 -0300

    Finalização do Contador de Pessoas

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,37 @@ void main() {
   runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Contador de Pessoas",
-      home: Stack(
+      home: Home()));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _people = 0;
+  String _lotation = "Pode Entrar";
+
+  void _changePeople(int delta) {
+    setState(() {
+      _people += delta;
+
+      if (_people >= 10) {
+        _people = 10;
+        _lotation = "Restaurante Cheio";
+      } else if (_people < 0) {
+        _people = 0;
+      } else {
+        _lotation = "Pode Entrar";
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
         children: <Widget>[
           Image.asset(
             "images/restaurante.jpg",
@@ -15,28 +45,16 @@ void main() {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Text(
-                "Pessoas: 0",
+                "Pessoas: $_people",
                 style: TextStyle(
                   color: Colors.white,
                   fontWeight: FontWeight.bold,
+                  fontSize: 35,
                 ),
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.all(10),
-                    child: FlatButton(
-                      child: Text(
-                        "+1",
-                        style: TextStyle(
-                          fontSize: 40,
-                          color: Colors.white,
-                        ),
-                      ),
-                      onPressed: () {},
-                    ),
-                  ),
                   Padding(
                     padding: EdgeInsets.all(10),
                     child: FlatButton(
@@ -47,13 +65,32 @@ void main() {
                           color: Colors.white,
                         ),
                       ),
-                      onPressed: () {},
+                      onPressed: () {
+                        _changePeople(-1);
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: FlatButton(
+                      child: Text(
+                        "+1",
+                        style: TextStyle(
+                          fontSize: 40,
+                          color: Colors.white,
+                        ),
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _changePeople(1);
+                        });
+                      },
                     ),
                   ),
                 ],
               ),
               Text(
-                "Pode entrar",
+                _lotation,
                 style: TextStyle(
                   color: Colors.white,
                   fontStyle: FontStyle.italic,
@@ -63,5 +100,7 @@ void main() {
             ],
           ),
         ],
-      )));
+      ),
+    );
+  }
 }
