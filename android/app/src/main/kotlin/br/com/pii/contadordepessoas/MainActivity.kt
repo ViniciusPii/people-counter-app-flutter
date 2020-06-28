33a2c5763a8e0fb64c7baf9b621944067606deb6
Vinicius <vinicius@iMac-de-Vinicius.local>
2020-06-28 16:46:19 -0300

    Inicio do Projeto

---
+++ android/app/src/main/kotlin/br/com/pii/contadordepessoas/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.pii.contadordepessoas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "Contador de Pessoas",
+    home: Container(
+      color: Colors.white,
+    ),
+  ));
+}
+
+
+
